03197f1d62be444a4da90f4e32b4631d42ad1556
Steffen Beginn <115171161+sBeginn@users.noreply.github.com>
2023-02-07 18:27:31 +0100

    Add files via upload

---
+++ test_fairsharer.py
@@ -0,0 +1,5 @@
+from fairsharer import fair_sharer
+
+def test_fair_sharer():
+    fair_sharer([0, 1000, 800, 0], 1)
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0], "Unexpected result."
